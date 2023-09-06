bc547c70e884e2a2c84495e7d421898578a729bb
DaniDhax <DaniDhax@users.noreply.github.com>
2023-09-06 13:56:13 +0200

    Design 4

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-const adviceIdNumber = document.getElementById("advice-id-number")
-const adviceText = document.getElementById("advice-text")
+const adviceIdNumber = document.getElementById("advice-id-number");
+const adviceText = document.getElementById("advice-text");
 const btnNuevaCita = document.getElementById("btn-nueva-cita");
 
-function traerNuevaCita() {
-  fetch("https://api.adviceslip.com/advice")
-    .then(res => {
-        // Verificar si la solicitud fue exitosa (código de estado 200)
-        if (!res.ok) {
-            throw new Error("Hubo un error en la solicitud");
-        }
-        // Analizar la respuesta JSON
-        return res.json();
-     })
-     .then(data => {
-        // Manipular los datos obtenidos de la API aquí
-        adviceIdNumber.innerText = data.slip.id
-        adviceText.innerText = data.slip.advice
-     })
-     .catch(err => {
-        // Manejar cualquier error que pueda ocurrir durante la solicitud
-        console.error("Ocurrió un error", err)
-     })
+async function traerNuevaCita() {
+  try {
+    const res = await fetch("https://api.adviceslip.com/advice");
+
+    // Verificar si la solicitud fue exitosa (código de estado 200)
+    if (!res.ok) {
+      throw new Error("Hubo un error en la solicitud");
+    }
+    
+    const data = await res.json();
+
+    // Manipular los datos obtenidos de la API aquí
+    adviceIdNumber.innerText = data.slip.id;
+    adviceText.innerText = data.slip.advice;
+  } catch (err) {
+    console.error("Ocurrió un error", err);
+  }
 }
 
 btnNuevaCita.addEventListener("click", traerNuevaCita);
+
